2a70c1828e9cd0680d1fcdeee219f38869888bdc
Marcos Warmling Berti <marcos_wb@outlook.com>
2025-10-22 21:19:44 -0300

    atualizar estrutura

--- src/scrapers/BBC.py
+++ src/scrapers/BBC.py
@@ -1,30 +1,29 @@
 from src.scrapers.base import BaseScraper
 
-class BBC(BaseScraper):
+class BBC:
 
     def __init__(self):
-        super().__init__('https://www.bbc.com')
+        self.__base_url = 'https://www.bbc.com'
 
     def extract(self):
-        base_url = self.get_website_link()
+        main_page = BaseScraper(self.__base_url)
+        main_page.load_page()
 
-        self.load_page()
-
-        for item in self.get_itens('a.sc-8a623a54-0'):
+        for item in main_page.get_itens('a.sc-8a623a54-0'):
             link = str(item['href'])
-            if self.is_link(link):
-                if link.startswith(base_url):
-                    check_url = link.replace(base_url, '')
-                    if not self.is_internal_link(check_url):
+            if main_page.is_link(link):
+                if link.startswith(self.__base_url):
+                    check_url = link.replace(self.__base_url, '')
+                    if not main_page.is_internal_link(check_url):
                         continue
 
                 url = str(link)
-            elif self.is_internal_link(link):
-                url = f'{base_url}{link}'
+            elif main_page.is_internal_link(link):
+                url = f'{self.__base_url}{link}'
             else:
                 continue
 
-            if not url.startswith(base_url):
+            if not url.startswith(self.__base_url):
                 continue
 
             print(url)
